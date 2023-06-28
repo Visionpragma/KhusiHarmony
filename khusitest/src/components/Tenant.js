fa5e15bb0351c8ade27af5e06879b3351dee2f4a
Visionpragma <135094767+Visionpragma@users.noreply.github.com>
2023-06-28 19:58:29 +0530

    toastify got added

--- khusitest/src/components/Tenant.js
+++ khusitest/src/components/Tenant.js
@@ -1,34 +1,40 @@
 import React, { useState } from 'react';
-import { TextField,makeStyles } from '@mui/material';
+import { TextField,makeStyles,Button } from '@mui/material';
 import { Container } from 'react-bootstrap';
+import { json } from 'react-router-dom';
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+  
+
+
+
 
 function RegistrationPage() {
   const [name, setName] = useState('');
   const [mobileNo, setMobileNo] = useState('');
   const [address, setAddress] = useState('');
+  
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
 
-  const handleMobileNoChange = (event) => {
-    setMobileNo(event.target.value);
-  };
-
-  const handleAddressChange = (event) => {
-    setAddress(event.target.value);
-  };
+    
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const tenant={name,mobileNo,address}
+    console.log(tenant)
+        fetch("http://localhost:8080/tenant/add",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(tenant)
+    }).then((res)=> {console.log("New Tenant Got added")
+        toast.success("Wow Tenant got added successfully!")
+   
+    })
+  }
     // Perform registration logic with the form data
     // For example, send an API request to register the user
-    console.log('Registration data:', { name, mobileNo, address });
-    // Reset the form fields
-    setName('');
-    setMobileNo('');
-    setAddress('');
-  };
+
 
   return (
     <div>
@@ -36,9 +42,10 @@ function RegistrationPage() {
       <Container>
       <form className='classes.root' noValidate autoComplete='off'>
         <TextField id="outlined-basic" label="FlatNo" fullWidth/>
-        <TextField id="outlined-basic" label="Name" fullWidth/>
-        <TextField id="outlined-basic" label="MobileNo" fullWidth/>
-        <TextField id="outlined-basic" label="Address" fullWidth/>
+        <TextField id="outlined-basic" label="Name"   fullWidth value={name} onChange={(Event)=>setName(Event.target.value)}/>
+        <TextField id="outlined-basic" label="MobileNo"  fullWidth value={mobileNo} onChange={(Event)=>setMobileNo(Event.target.value)}/>
+        <TextField id="outlined-basic" label="Address" fullWidth value={address} onChange={(Event)=>setAddress(Event.target.value)}/>
+        <Button variant='contained' color='secondary'onClick={handleSubmit}>Submit</Button>
 
       </form>
       </Container>
